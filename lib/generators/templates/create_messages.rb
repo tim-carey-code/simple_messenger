566acb38c2beb0d5dc082c1302f56642faee7211
timcarey <timcarey1989@gmail.com>
2024-05-14 15:52:30 -0400

    fix AR version in migration

--- lib/generators/templates/create_messages.rb
+++ lib/generators/templates/create_messages.rb
@@ -1,16 +1,16 @@
-class CreateMessages < ActiveRecord::Migration
+class CreateMessages < ActiveRecord::Migration[7.1]
   def change
     create_table :messages do |t|
       t.references :sender, polymorphic: true, null: false
       t.references :receiver, polymorphic: true, null: false
       t.string :subject
-      t.text :content, :null => false
-      t.boolean :viewed, :null => false, :default => false
+      t.text :content, null: false
+      t.boolean :viewed, null: false, default: false
 
       t.timestamps
     end
 
-    add_index :messages, [:sender_id, :sender_type]
-    add_index :messages, [:receiver_id, :receiver_type]
+    add_index :messages, %i[sender_id sender_type]
+    add_index :messages, %i[receiver_id receiver_type]
   end
 end
